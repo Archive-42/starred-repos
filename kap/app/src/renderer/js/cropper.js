46a19339de88f09b3d4e8f371a6ceb6f82366898
Bryan C Guner <bryan.guner@gmail.com>
2021-12-11 18:56:31 +0000

    update

---
+++ kap/app/src/renderer/js/cropper.js
@@ -0,0 +1,66 @@
+import {ipcRenderer} from 'electron';
+
+import {init as initErrorReporter} from '../../common/reporter';
+
+const arrows = {
+  left: 'ArrowLeft',
+  up: 'ArrowUp',
+  right: 'ArrowRight',
+  down: 'ArrowDown'
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  function autoDestroy() {
+    ipcRenderer.send('close-cropper-window');
+  }
+
+  function move(direction, amount) {
+    ipcRenderer.send('move-cropper-window', {direction, amount});
+  }
+
+  let intervalId;
+  let timeoutId;
+
+  function keyUp(event) {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+      timeoutId = undefined;
+    }
+
+    if (intervalId) {
+      clearInterval(intervalId);
+      intervalId = undefined;
+    }
+
+    switch (event.key) {
+      case 'Escape':
+        autoDestroy();
+        break;
+      case 'Enter':
+        ipcRenderer.send('start-recording');
+        break;
+      case ' ':
+        ipcRenderer.send('start-recording');
+        break;
+      default:
+        break;
+    }
+  }
+
+  function keyDown(event) {
+    if (!timeoutId && !intervalId) {
+      const direction = Object.keys(arrows).find(key => arrows[key] === event.key);
+      const amount = event.shiftKey ? 10 : 1;
+      if (direction) {
+        move(direction, amount);
+        timeoutId = setTimeout(() => {
+          intervalId = setInterval(() => move(direction, amount), 50);
+        }, 250);
+      }
+    }
+  }
+
+  window.addEventListener('keyup', keyUp, false);
+  window.addEventListener('keydown', keyDown, false);
+  initErrorReporter();
+});
