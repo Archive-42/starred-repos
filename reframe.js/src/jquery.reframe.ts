e251f2450e63417c3cea645450025bbc9e79a3f1
Bryan C Guner <bryan.guner@gmail.com>
2021-12-11 19:08:05 +0000

    update

---
+++ reframe.js/src/jquery.reframe.ts
@@ -0,0 +1,10 @@
+import reframe from './reframe'
+
+if (typeof window !== 'undefined') {
+  const plugin = window.$ || window.jQuery || window.Zepto
+  if (plugin) {
+    plugin.fn.reframe = function reframePlugin (cName) {
+      reframe(this, cName)
+    }
+  }
+}
