933baa814977c95e53d28d7f8959c8121aa5c0b6
Bryan C Guner <bryan.guner@gmail.com>
2021-12-11 18:38:54 +0000

    update

---
+++ Back-End/api/events/events-middleware.js
@@ -0,0 +1,96 @@
+// const Events = require("./events-model");
+const jwt = require("jsonwebtoken");
+const Events = require("./events-model");
+const Users = require("../auth/auth-model");
+const { eventSchema, guestSchema, itemSchema } = require("../schemaValidation");
+const JWT_SECRET = require("../secrets");
+
+const bodyValidation = async (req, res, next) => {
+  try {
+    req.body = await eventSchema.validate(req.body, {
+      stripUnknown: true,
+    });
+    next();
+  } catch (err) {
+    next({ status: 400, message: err.message });
+  }
+};
+
+const restriction = (req, res, next) => {
+  const token = req.headers.authorization;
+  jwt.verify(token, JWT_SECRET, (err, decoded) => {
+    if (err) {
+      next({ status: 401, message: "Token authentication error" });
+    } else {
+      req.decoded = decoded;
+      next();
+    }
+  });
+};
+
+const only = (req, res, next) => {
+  if (req.decoded.subject === req.params.user_id) next();
+  else
+    next({ status: 403, message: "You are not allowed access to this data" });
+};
+
+const checkUserId = async (req, res, next) => {
+  try {
+    const { user_id } = req.params;
+    if (!user_id) {
+      next({ status: 400, message: "must include a user_id" });
+    }
+    const check = await Users.findBy({ user_id });
+    if (!check) {
+      next({ status: 400, message: "user ID does not exist!" });
+    } else {
+      next();
+    }
+  } catch (err) {
+    next({ status: 400, message: "user ID does not exist!" });
+  }
+};
+
+const checkEventId = async (req, res, next) => {
+  try {
+    const { event_id } = req.params;
+    const check = await Events.getBy({ event_id }).first();
+    if (!check) {
+      next({ status: 400, message: "Event ID does not exist!" });
+    } else {
+      next();
+    }
+  } catch (err) {
+    next({ status: 400, message: "Event ID does not exist!" });
+  }
+};
+
+const checkGuestBody = async (req, res, next) => {
+  try {
+    req.body = await guestSchema.validate(req.body, {
+      stripUnknown: true,
+    });
+    next();
+  } catch (err) {
+    next({ status: 400, message: err.message });
+  }
+};
+const checkItemBody = async (req, res, next) => {
+  try {
+    req.body = await itemSchema.validate(req.body, {
+      stripUnknown: true,
+    });
+    next();
+  } catch (err) {
+    next({ status: 400, message: err.message });
+  }
+};
+module.exports = {
+  bodyValidation,
+  restriction,
+  only,
+  checkUserId,
+  checkEventId,
+  checkGuestBody,
+  checkItemBody,
+};
---
+++ Back-End/api/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const { restriction } = require("./events/events-middleware");
+
+const server = express();
+server.use(express.json());
+server.use(helmet());
+server.use(cors());
+
+server.use("/api/auth", require("./auth/auth-router"));
+server.use("/api/events", restriction, require("./events/events-router"));
+
+server.use("*", (res, req /*eslint-disable-line */) => {
+  res.status(404).json({ message: "invalid endpoint!" });
+});
+
+server.use((err, req, res, next /*eslint-disable-line */) => {
+  res.status(err.status).json({
+    note: "Houston, We have a problem!!!",
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = server;
