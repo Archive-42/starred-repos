2db4082681691c6b6792987a234fb91c172e5d3c
Bryan C Guner <bryan.guner@gmail.com>
2021-12-11 18:40:25 +0000

    update

---
+++ blog-code-samples/ports-and-adapters/src/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ports-and-adapters',
+    url='https://github.com/bobthemighty/ports-and-adapters-pythong',
+    packages=find_packages('.'),
+)
