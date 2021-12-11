d33ef4eb262f06ebbdc8f0ad25cf033547dbbdd8
Bryan C Guner <bryan.guner@gmail.com>
2021-12-11 19:04:13 +0000

    update

---
+++ pnpm.github.io/benchmarks/commandsMap.js
@@ -0,0 +1,53 @@
+'use strict'
+
+module.exports = {
+  npm: {
+    scenario: 'npm',
+    legend: 'npm',
+    name: 'npm',
+    args: [
+      'install',
+      '--no-fund',
+      '--no-audit',
+      '--ignore-scripts',
+      '--cache',
+      'cache',
+      '--registry',
+      'https://registry.npmjs.org/'
+    ]
+  },
+  pnpm: {
+    scenario: 'pnpm',
+    legend: 'pnpm',
+    name: 'pnpm',
+    args: [
+      'install',
+      '--ignore-scripts',
+      '--store-dir',
+      `cache`,
+      '--registry',
+      'https://registry.npmjs.org/'
+    ]
+  },
+  yarn: {
+    scenario: 'yarn',
+    legend: 'Yarn',
+    name: 'yarn',
+    args: [
+      '--ignore-scripts',
+      '--cache-folder',
+      'cache'
+    ]
+  },
+  yarn_pnp: {
+    scenario: 'yarn_pnp',
+    legend: "Yarn PnP",
+    name: 'yarn',
+    args: [
+      '--pnp',
+      '--ignore-scripts',
+      '--cache-folder',
+      'cache'
+    ]
+  }
+}
---
+++ pnpm.github.io/docusaurus.config.js
@@ -0,0 +1,226 @@
+const path = require('path');
+
+const GITHUB_URL = 'https://github.com/pnpm/pnpm';
+const SPONSOR_URL = 'https://opencollective.com/pnpm';
+const TRANSLATE_URL = "https://crowdin.com/project/pnpm";
+const DEFAULT_LOCALE = 'en';
+const LOCALE_FULL_CODE = {
+  zh: 'zh-CN',
+}
+
+function makeEditUrl (locale, path1, path2) {
+  // Link to Crowdin for non-English docs
+  if (locale !== DEFAULT_LOCALE) {
+    return `https://crowdin.com/project/pnpm/${LOCALE_FULL_CODE[locale] || locale}`;
+  }
+  // Link to Github for English docs
+  return `https://github.com/pnpm/pnpm.github.io/edit/source/${path1}/${path2}`;
+}
+
+module.exports={
+  "title": "pnpm",
+  "tagline": "Fast, disk space efficient package manager",
+  "url": "https://pnpm.io",
+  "baseUrl": "/",
+  "organizationName": "pnpm",
+  "projectName": "pnpm.github.io",
+  "scripts": [
+    "https://buttons.github.io/buttons.js"
+  ],
+  "favicon": "img/favicon.png",
+  "customFields": {
+    "users": require('./users.json'),
+    "repoUrl": GITHUB_URL,
+    "translationRecruitingLink": TRANSLATE_URL,
+  },
+  "onBrokenLinks": "log",
+  "onBrokenMarkdownLinks": "log",
+  "presets": [
+    [
+      "@docusaurus/preset-classic",
+      {
+        "docs": {
+          "showLastUpdateAuthor": true,
+          "showLastUpdateTime": true,
+          editUrl: ({locale, versionDocsDirPath, docPath}) => makeEditUrl(locale, versionDocsDirPath, docPath),
+          "path": "./docs",
+          "routeBasePath": "/",
+          "sidebarPath": path.join(__dirname, "sidebars.json"),
+        },
+        "blog": {
+          "path": "blog",
+          editUrl: ({locale, blogDirPath, blogPath}) => makeEditUrl(locale, blogDirPath, blogPath),
+        },
+        "theme": {
+          "customCss": "../src/css/customTheme.css"
+        }
+      }
+    ]
+  ],
+  "plugins": [
+    [
+      '@docusaurus/plugin-client-redirects',
+      {
+        createRedirects: (existingPath) => {
+          const alternatives = []
+          if (!existingPath.match(/^\/[a-z]{2}\//)) {
+            alternatives.push(`/en${existingPath}`);
+          }
+          if (!existingPath.startsWith('/5.x/')) {
+            alternatives.push(`/5.x${existingPath}`);
+          }
+          return alternatives;
+        }
+      }
+    ]
+  ],
+  "themeConfig": {
+    "prism": {
+      theme: require('prism-react-renderer/themes/github'),
+      darkTheme: require('prism-react-renderer/themes/dracula'),
+    },
+    "navbar": {
+      "title": "pnpm",
+      "logo": {
+        "src": "img/pnpm-no-name-with-frame.svg"
+      },
+      "items": [
+        {
+          "to": "motivation",
+          "label": "Docs",
+          "position": "left"
+        },
+        {
+          "to": "blog",
+          "label": "Blog",
+          "position": "left"
+        },
+        {
+          "to": "/faq",
+          "label": "FAQ",
+          "position": "left"
+        },
+        {
+          "to": "/benchmarks",
+          "label": "Benchmarks",
+          "position": "left"
+        },
+        {
+          type: 'docsVersionDropdown',
+          "position": "right",
+        },
+        {
+          type: 'localeDropdown',
+          position: 'right',
+          dropdownItemsAfter: [
+            {
+              to: 'https://crowdin.com/project/pnpm',
+              label: 'Help Us Translate',
+            },
+          ],
+        },
+        {
+          href: SPONSOR_URL,
+          position: 'right',
+          label: 'Sponsor Us'
+        },
+        {
+          href: 'https://github.com/pnpm/pnpm',
+          position: 'right',
+          className: 'header-github-link',
+          'aria-label': 'GitHub repository',
+        },
+      ],
+    },
+    "image": "img/ogimage.png",
+    "footer": {
+      "links": [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Getting Started',
+              to: 'installation'
+            },
+            {
+              label: 'pnpm CLI',
+              to: 'pnpm-cli',
+            },
+            {
+              label: 'Workspace',
+              to: 'workspaces',
+            },
+            {
+              label: '.npmrc',
+              to: 'npmrc',
+            },
+          ]
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'User Showcase',
+              to: 'users'
+            },
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/pnpm'
+            },
+            {
+              label: 'Project Chat',
+              href: 'https://r.pnpm.io/chat',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/pnpmjs'
+            }
+          ]
+        },
+        {
+          title: 'Contributing',
+          items: [
+            {
+              label: 'GitHub',
+              to: GITHUB_URL,
+            },
+            {
+              label: 'Sponsor Us',
+              to: SPONSOR_URL,
+            },
+            {
+              label: 'Help Us Translate',
+              to: TRANSLATE_URL,
+            },
+          ],
+        },
+      ],
+      "copyright": "Copyright © 2015-2021 contributors of pnpm",
+      "logo": {
+        "src": "img/pnpm-light.svg"
+      },
+      "style": "dark",
+    },
+    "algolia": {
+      "apiKey": "802b1d6ee5450a4296581a8e14aff417",
+      "indexName": "pnpm",
+      "contextualSearch": true,
+    },
+    "gtag": {
+      trackingID: "UA-91385296-1",
+      anonymizeIP: true,
+    }
+  },
+  i18n: {
+    defaultLocale: DEFAULT_LOCALE,
+    locales: ['en', 'zh', 'es', 'ru', 'tr', 'hu'],
+    localeConfigs: {
+      en: { label: 'English' },
+      es: { label: 'Español' },
+      ru: { label: 'Русский' },
+      zh: { label: '中文' },
+      tr: { label: 'Türkçe' },
+      hu: { label: 'Magyar' },
+    },
+  },
+}
