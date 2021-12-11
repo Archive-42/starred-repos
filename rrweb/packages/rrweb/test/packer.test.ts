d690074d88d02ee55e1960b0e3cbce8d5581ba64
Bryan C Guner <bryan.guner@gmail.com>
2021-12-11 19:08:58 +0000

    update

---
+++ rrweb/packages/rrweb/test/packer.test.ts
@@ -0,0 +1,41 @@
+import { pack, unpack } from '../src/packer';
+import { eventWithTime, EventType } from '../src/types';
+import { MARK } from '../src/packer/base';
+
+const event: eventWithTime = {
+  type: EventType.DomContentLoaded,
+  data: {},
+  timestamp: new Date('2020-01-01').getTime(),
+};
+
+describe('pack', () => {
+  it('can pack event', () => {
+    const packedData = pack(event);
+    expect(packedData).toMatchSnapshot();
+  });
+});
+
+describe('unpack', () => {
+  it('is compatible with unpacked data 1', () => {
+    const result = unpack((event as unknown) as string);
+    expect(result).toEqual(event);
+  });
+
+  it('is compatible with unpacked data 2', () => {
+    const result = unpack(JSON.stringify(event));
+    expect(result).toEqual(event);
+  });
+
+  it('stop on unknown data format', () => {
+    expect(() => unpack('[""]')).toThrow('');
+  });
+
+  it('can unpack packed data', () => {
+    const packedData = pack(event);
+    const result = unpack(packedData);
+    expect(result).toEqual({
+      ...event,
+      v: MARK,
+    });
+  });
+});
